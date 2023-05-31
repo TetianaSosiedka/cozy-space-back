dfeddf576856f993e3fc3c20acc97fc9f8505e0b
TetianaSosiedka <tasha.sosedka@gmail.com>
2023-05-31 18:44:11 +0300

    Update getProducts.js

--- controllers/products/getProducts.js
+++ controllers/products/getProducts.js
@@ -4,12 +4,14 @@ const { KECRM_PATH, KECRM_API_KEY } = process.env;
 const getProducts = async (req, res) => {
   const url = KECRM_PATH;
   const token = KECRM_API_KEY;
-  axios
-    .get(url, {
+  const limit = 15;
+  const { page = 1 } = req.query;
+
+  await axios
+    .get(`${url}?limit=${limit}&page=${page}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
     .then((response) => {
-      console.log(response.data);
       res.json(response.data);
     })
     .catch((error) => {
